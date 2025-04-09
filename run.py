422f85f69646f63f9e44b18add943461edbe081e
TsungHanChou <wt1402021@gmail.com>
2025-04-10 00:49:37 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,15 @@
 import polars as pl
 from polars_snowball_stemmer import snowball_stem
 
-df = pl.DataFrame({'word': ["fearlessly", "littleness", "lovingly", "devoted"]})
-print(df.with_columns(b=mp.snowball_stem('word')))
+df = pl.DataFrame({
+    'word': [
+        "hopelessly", 
+        "fearfulness", 
+        "amazingly", 
+        "careless", 
+        "beautifully", 
+        "kindness"
+    ]
+})
+
+print(df.with_columns(b=snowball_stem('word')))
